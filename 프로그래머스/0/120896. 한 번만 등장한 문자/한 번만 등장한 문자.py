2bcb264448c691ac6c924181464edaf896477dec
오채은 <81213566+chean5o@users.noreply.github.com>
2024-05-05 22:43:40 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -0,0 +1,6 @@
+def solution(s):
+    char_count = {}
+    for char in s:
+        char_count[char] = char_count.get(char, 0) + 1
+    unique_chars = [char for char, count in char_count.items() if count == 1]
+    return ''.join(sorted(unique_chars))
