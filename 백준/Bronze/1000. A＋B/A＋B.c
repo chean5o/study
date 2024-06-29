4005204e427e4dcdc75b333bcfe2cdf10e37abc0
오채은 <81213566+chean5o@users.noreply.github.com>
2024-06-29 22:55:21 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int A, B;
+    int sum;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    sum = A + B;
+    printf("%d", sum);
+    
+    return 0;
+}
