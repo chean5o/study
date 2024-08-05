daddd5d3e55f3c5977697c487a8385d9557e98be
오채은 <81213566+chean5o@users.noreply.github.com>
2024-08-05 21:31:33 +0900

    [Silver III] Title: 두 스티커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16937. 두 스티커/두 스티커.py
+++ 백준/Silver/16937. 두 스티커/두 스티커.py
@@ -1,26 +1,29 @@
 H, W = map(int, input().split())
 N = int(input())
 sticker = [list(map(int, input().split())) for i in range(N)]
-max_area = 0
-for i in range(N):
-  for j in range(i+1, N):
-    r1, c1 = sticker[i]
-    r2, c2 = sticker[j]
 
-    # 첫 번째 스티커의 방향 그대로, 두 번째 스티커의 방향 그대로
+
+def can_place(r1, c1, r2, c2):
     if (r1 + r2 <= H and max(c1, c2) <= W) or (c1 + c2 <= W and max(r1, r2) <= H):
-        max_area = max(max_area, r1 * c1 + r2 * c2)
-    
-    # 첫 번째 스티커의 방향 그대로, 두 번째 스티커를 90도 회전
+        return True
     if (r1 + c2 <= H and max(c1, r2) <= W) or (c1 + r2 <= W and max(r1, c2) <= H):
-        max_area = max(max_area, r1 * c1 + r2 * c2)
-    
-    # 첫 번째 스티커를 90도 회전, 두 번째 스티커의 방향 그대로
-    if (c1 + r2 <= H and max(r1, c2) <= W) or (r1 + c2 <= W and max(c1, r2) <= H):
-        max_area = max(max_area, r1 * c1 + r2 * c2)
-    
-    # 첫 번째 스티커를 90도 회전, 두 번째 스티커를 90도 회전
-    if (c1 + c2 <= H and max(r1, r2) <= W) or (r1 + r2 <= W and max(c1, c2) <= H):
-        max_area = max(max_area, r1 * c1 + r2 * c2)
+        return True
+    return False
+
+max_area = 0
+
+for i in range(N):
+    for j in range(i+1, N):
+        r1, c1 = sticker[i]
+        r2, c2 = sticker[j]
+        
+        if can_place(r1, c1, r2, c2):
+            max_area = max(max_area, r1*c1 + r2*c2)
+        if can_place(r1, c1, c2, r2):
+            max_area = max(max_area, r1*c1 + r2*c2)
+        if can_place(c1, r1, r2, c2):
+            max_area = max(max_area, r1*c1 + r2*c2)
+        if can_place(c1, r1, c2, r2):
+            max_area = max(max_area, r1*c1 + r2*c2)
 
 print(max_area)
