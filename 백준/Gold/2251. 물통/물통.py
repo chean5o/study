35187477b280b50b51185d5e74fd55c2961959df
오채은 <81213566+chean5o@users.noreply.github.com>
2024-08-12 22:53:19 +0900

    [Gold V] Title: 물통, Time: 32 ms, Memory: 31164 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.py
+++ 백준/Gold/2251. 물통/물통.py
@@ -1,54 +1,55 @@
 a, b, c = map(int, input().split())
-
-visited = [[0 for _ in range(b + 1)] for _ in range(a + 1)]
-possible_c = set()  # 세 번째 물통에 담길 수 있는 물의 양 저장
+visited = []
+bottle_c = set()
+for _ in range(a + 1):
+    visited.append([0] * (b + 1))
 
 def dfs(n_a, n_b):
-    if visited[n_a][n_b] == 0:
-        visited[n_a][n_b] = 1
-        n_c = c - n_a - n_b  # 현재 세 번째 물통에 남아있는 물의 양
-        
-        if n_a == 0:  # 첫 번째 물통이 비어있는 경우, 세 번째 물통의 물 양 기록
-            possible_c.add(n_c)
+  if visited[n_a][n_b] == 0:
+    visited[n_a][n_b] = 1
+    n_c = c - n_a - n_b # 현재 세번째 물통에 남아있는 물의 양
+    if n_a == 0:
+      bottle_c.add(n_c)
 
-        # A -> B
-        if n_a + n_b <= b:
-            dfs(0, n_a + n_b)
-        else:
-            dfs(n_a + n_b - b, b)
+    # a -> b
+    if n_a + n_b < b : # 첫번째 + 두번째 물통의 물의 양 < 두번째 물통의 최대 용량
+      dfs(0, n_a + n_b)
+    else: # 첫번째 + 두번째 물통의 물의 양 > 두번째 물통의 최대 용량
+      dfs(n_a + n_b - b, b)
+    
+    # a -> c
+    if n_a + n_c <= c:
+        dfs(0, n_b)
+    else:
+        dfs(n_a + n_c - c, n_b)
 
-        # A -> C
-        if n_a + n_c <= c:
-            dfs(0, n_b)
-        else:
-            dfs(n_a + n_c - c, n_b)
+    # b -> a
+    if n_b + n_a <= a:
+        dfs(n_b + n_a, 0)
+    else:
+        dfs(a, n_b + n_a - a)
 
-        # B -> A
-        if n_b + n_a <= a:
-            dfs(n_b + n_a, 0)
-        else:
-            dfs(a, n_b + n_a - a)
+    # b -> c
+    if n_b + n_c <= c:
+        dfs(n_a, 0)
+    else:
+        dfs(n_a, n_b + n_c - c)
 
-        # B -> C
-        if n_b + n_c <= c:
-            dfs(n_a, 0)
-        else:
-            dfs(n_a, n_b + n_c - c)
+    # c -> a
+    if n_c + n_a <= a:
+        dfs(n_c + n_a, n_b)
+    else:
+        dfs(a, n_b)
 
-        # C -> A
-        if n_c + n_a <= a:
-            dfs(n_c + n_a, n_b)
-        else:
-            dfs(a, n_b)
+    # c -> a
+    if n_c + n_b <= b:
+        dfs(n_a, n_c + n_b)
+    else:
+        dfs(n_a, b)
 
-        # C -> B
-        if n_c + n_b <= b:
-            dfs(n_a, n_c + n_b)
-        else:
-            dfs(n_a, b)
+dfs(0,0) # 초기 값 첫번째와 두번째 물통에는 물이 없는 상태
 
-dfs(0, 0)  # 초기 상태로 DFS 시작
+res = sorted(bottle_c)
 
-# 가능한 세 번째 물통의 물의 양을 정렬하여 출력
-res = sorted(possible_c)
-print(*res)
+for i in res:
+    print(i, end=" ")
